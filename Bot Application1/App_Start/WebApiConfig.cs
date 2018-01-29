6dc0e09ce950df3c513b238ae9f0eed187dcc9d3
Amilton Faria Silva <35498695+AmiltonFariaSilva@users.noreply.github.com>
2018-01-29 16:53:09 -0200

    ChatBot
    Criação de um ChatBot com as APIs da Microsoft

---
+++ Bot Application1/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Bot_Application1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Json settings
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+
+            // Web API configuration and services
+            //POST / knowledgebases / 85166011 - 7579 - 4896 - be1b - 33f52c651ad9 / generateAnswer
+            //Host: https://westus.api.cognitive.microsoft.com/qnamaker/v2.0
+            //Ocp - Apim - Subscription - Key: 58523c2cdcea407c989b2f3fb8609862
+            //Content - Type: application / json
+            //{ "question":"hi"}
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Bot Application1/Dialogs/QnADialogWithActiveLearning.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    [QnAMaker("58523c2cdcea407c989b2f3fb8609862", "85166011-7579-4896-be1b-33f52c651ad9", "Não identifiquei sua pergunta", 0.5, 1)]
+    public class QnADialogWithActiveLearning : QnAMakerDialog
+    {
+
+    }
+}
